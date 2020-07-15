32c7b361c6324a2ce2ff83a888f219fd42ba4ddb
Ambtuano <36522174+Ambtuano@users.noreply.github.com>
2020-07-15 12:58:50 -0400

    Delete EulerAngles.m

--- HORC/CODE/EulerAngles.m
+++
@@ -1,7 +0,0 @@
-function Euler = EulerAngles(T_total) %zyz
-phi = atan2(-T_total(2,3),-T_total(1,3));
-theta = atan2(-sqrt(T_total(1,3)^2 + T_total(2,3)^2),T_total(3,3));
-psi = atan2(-T_total(3,2),T_total(3,1));
-
-Euler = [phi; theta; psi];
- 
